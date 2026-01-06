5eb810a7c3f44bd18eca30a2d87095fa4fb559c6
MUKILTEO\1722453 <davewilson260@gmail.com>
2026-01-06 13:38:10 -0800

    added movement and cam controller and started making folders

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public float speed = 5f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.fixedDeltaTime * speed);
+    }
+
+}
