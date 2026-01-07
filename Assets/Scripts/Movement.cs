833effeea3bc0d1faef31d483a1d94e19d2bb6de
DESKTOP-6HR0FGG\davew <davewilson260@gmail.com>
2026-01-06 16:07:53 -0800

    working on animation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Vector2 targetPosition;
+    public Rigidbody2D _playerRB2D;
+
+    private Animator _anim;
 
     // Update is called once per frame
     void Update()
@@ -13,8 +16,13 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _anim.SetFloat("x", _playerRB2D.linearVelocityX);
+            _anim.SetFloat("y", _playerRB2D.linearVelocityY);
         }
 
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+
+        
+
     }
 }
